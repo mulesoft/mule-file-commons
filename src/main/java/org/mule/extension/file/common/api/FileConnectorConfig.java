5b3bf94a256633735fbb8ebf0b4a52373988b92f
ndinu <ndinu@users.noreply.github.com>
2018-07-04 14:08:02 -0300

    MULE-14880: FTP getting partial file (#41)

--- src/main/java/org/mule/extension/file/common/api/FileConnectorConfig.java
+++ src/main/java/org/mule/extension/file/common/api/FileConnectorConfig.java
@@ -6,12 +6,16 @@
  */
 package org.mule.extension.file.common.api;
 
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
 import static org.mule.runtime.extension.api.annotation.param.display.Placement.ADVANCED_TAB;
 import org.mule.runtime.extension.api.annotation.param.DefaultEncoding;
 import org.mule.runtime.extension.api.annotation.param.Optional;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
 import org.mule.runtime.extension.api.annotation.param.RefName;
 import org.mule.runtime.extension.api.annotation.param.display.Placement;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Generic contract for a config of a connector which operates over a {@link FileSystem}
@@ -44,5 +48,14 @@ public abstract class FileConnectorConfig {
 
   public String getDefaultWriteEncoding() {
     return defaultWriteEncoding == null ? muleEncoding : defaultWriteEncoding;
+  }
+
+  /**
+   * @param timeBetweenSizeCheck      amount of time units.
+   * @param timeBetweenSizeCheckUnit  {@link TimeUnit} that will be converted to milliseconds.
+   * @return {@link Long} representing an amount of millisecond or null if {@param timeBetweenSizeCheck} is null
+   */
+  public java.util.Optional<Long> getTimeBetweenSizeCheckInMillis(Long timeBetweenSizeCheck, TimeUnit timeBetweenSizeCheckUnit) {
+    return timeBetweenSizeCheck == null ? empty() : of(timeBetweenSizeCheckUnit.toMillis(timeBetweenSizeCheck));
   }
 }
