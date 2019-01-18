0910a48ac10170d14f00317477396f8c8fc8072c
Nicolas Santiago Di Nucci <nico.dinu@gmail.com>
2019-01-18 12:01:07 -0300

    MULE-16346: FTP list operation takes too long

--- src/main/java/org/mule/extension/file/common/api/command/ReadCommand.java
+++ src/main/java/org/mule/extension/file/common/api/command/ReadCommand.java
@@ -47,4 +47,23 @@ public interface ReadCommand<A extends FileAttributes> {
   default Result<InputStream, A> read(FileConnectorConfig config, String filePath, boolean lock, Long timeBetweenSizeCheck) {
     return read(config, filePath, lock);
   }
+
+  /**
+   * Reads files under the considerations of {@link FileSystem#read(FileConnectorConfig, String, boolean)} This method can be used
+   * instead of {@link ReadCommand#read(FileConnectorConfig, String, boolean, Long)} to avoid extra processing to get the file
+   * attributes again if that information is already collected (this is important if the attributes are gathered from a remote
+   * server).
+   *
+   * @param config the config that is parameterizing this operation
+   * @param attributes the attributes of the file you want to read
+   * @param lock whether or not to lock the file
+   * @param timeBetweenSizeCheck wait time between size checks to determine if a file is ready to be read in milliseconds.
+   * @return An {@link Result} with an {@link InputStream} with the file's content as payload and a {@link FileAttributes} object
+   *         as {@link Message#getAttributes()}
+   * @throws IllegalArgumentException if the file at the given path doesn't exist
+   */
+  default Result<InputStream, A> read(FileConnectorConfig config, A attributes, boolean lock, Long timeBetweenSizeCheck) {
+    return read(config, attributes.getPath(), lock, timeBetweenSizeCheck);
+  }
+
 }
