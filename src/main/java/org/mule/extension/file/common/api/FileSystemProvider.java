0580b0cfb6b6803f50dc5a262f8eaeb901097287
Juan Baca Paunero <Bardammu@users.noreply.github.com>
2019-04-03 16:38:16 -0300

    MULE-16640: Add missing equals and hashCode (#59)

--- src/main/java/org/mule/extension/file/common/api/FileSystemProvider.java
+++ src/main/java/org/mule/extension/file/common/api/FileSystemProvider.java
@@ -9,6 +9,8 @@ package org.mule.extension.file.common.api;
 import org.mule.runtime.api.connection.ConnectionProvider;
 import org.mule.runtime.extension.api.annotation.param.RefName;
 
+import java.util.Objects;
+
 /**
  * Base class for a {@link ConnectionProvider} which provides instances of
  * {@link FileSystem}
@@ -32,4 +34,23 @@ public abstract class FileSystemProvider<T extends FileSystem> implements Connec
    * The directory to be considered as the root of every relative path used with this connector.
    */
   public abstract String getWorkingDir();
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    FileSystemProvider<?> that = (FileSystemProvider<?>) o;
+    return Objects.equals(configName, that.configName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(configName);
+  }
 }
