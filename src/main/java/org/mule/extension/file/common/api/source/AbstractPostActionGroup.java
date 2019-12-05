4fa9471bead3ec30825dfa438d7862c742bc8c53
Gabriel Dal Borgo <gabrieldalborgo@gmail.com>
2019-12-05 18:06:28 -0300

    File Connectors listener post action should be able to rename file

--- src/main/java/org/mule/extension/file/common/api/source/AbstractPostActionGroup.java
+++ src/main/java/org/mule/extension/file/common/api/source/AbstractPostActionGroup.java
@@ -59,24 +59,33 @@ public abstract class AbstractPostActionGroup {
       }
     }
 
-    boolean moved = false;
+    boolean movedOrRenamed = false;
     try {
       if (getMoveToDirectory() != null) {
         fileSystem.move(config, fileAttributes.getPath(), getMoveToDirectory(), false, true,
                         getRenameTo());
-        moved = true;
+        movedOrRenamed = true;
+      } else if (getRenameTo() != null) {
+        fileSystem.rename(fileAttributes.getPath(), getRenameTo(), false);
+        movedOrRenamed = true;
       }
     } catch (FileAlreadyExistsException e) {
       if (!isAutoDelete()) {
-        String moveToFileName = getRenameTo() == null ? fileAttributes.getName() : getRenameTo();
-        String moveToPath = Paths.get(getMoveToDirectory()).resolve(moveToFileName).toString();
-        LOGGER.warn(String.format("A file with the same name was found when trying to move '%s' to '%s'" +
-            ". The file '%s' was not sent to the moveTo directory and it remains on the poll directory.",
-                                  fileAttributes.getPath(), moveToPath, fileAttributes.getPath()));
+        if (getMoveToDirectory() == null) {
+          LOGGER.warn(String.format("A file with the same name was found when trying to rename '%s' to '%s'" +
+                          ". The file '%s' was not renamed and it remains on the poll directory.",
+                  fileAttributes.getName(), getRenameTo(), fileAttributes.getPath()));
+        } else {
+          String moveToFileName = getRenameTo() == null ? fileAttributes.getName() : getRenameTo();
+          String moveToPath = Paths.get(getMoveToDirectory()).resolve(moveToFileName).toString();
+          LOGGER.warn(String.format("A file with the same name was found when trying to move '%s' to '%s'" +
+                          ". The file '%s' was not sent to the moveTo directory and it remains on the poll directory.",
+                  fileAttributes.getPath(), moveToPath, fileAttributes.getPath()));
+        }
         throw e;
       }
     } finally {
-      if (isAutoDelete() && !moved) {
+      if (isAutoDelete() && !movedOrRenamed) {
         fileSystem.delete(fileAttributes.getPath());
       }
     }
